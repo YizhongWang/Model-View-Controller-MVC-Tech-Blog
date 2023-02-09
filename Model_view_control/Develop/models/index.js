b17e1b4fa68d3ed787cd5d153213c7700ec59bcd
Yizhong Wang <wang.6689@osu.edu>
2023-02-09 01:02:50 -0500

    initial commit

---
+++ Model_view_control/Develop/models/index.js
@@ -0,0 +1,20 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment,{
+  foreignKey: 'blog_id'
+})
+Comment.belongsTo(User,{
+  foreignKey: 'user_id',
+})
+module.exports = { User, Project };
